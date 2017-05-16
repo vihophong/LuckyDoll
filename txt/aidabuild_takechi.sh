655fce1f7bec3807fe16989ba4c87fe007b7885e
vihophong <hophongmc@gmail.com>
2017-05-16 20:02:17 +0900

    new manual

---
+++ txt/aidabuild_takechi.sh
@@ -0,0 +1,23 @@
+#!/bin/bash                                                                
+
+# make file list                                                  
+#ls -tr /home/aida/raid/aida/rawdata/takechi_exp/R$1* | wc -l > list/list_R$1.txt
+#ls -tr /home/aida/raid/aida/rawdata/takechi_exp/R$1* >> list/list_R$1.txt
+
+if [ $# == 4 ]; then
+
+    expr $3 - $2 + 1 > list/list_R$4_$1_$2to$3.txt
+    for i in `seq $2 $3`;
+    do
+	ls -tr aidaraw/R$1_$i >> list/list_R$4_$1_$2to$3.txt
+    done  
+    # run
+    ./aidanew -a list/list_R$4_$1_$2to$3.txt -o rootfiles/aidatakechinew/$4_aida$1_$2to$3.root -map config/FEE_table_briken2016.txt -thr config/thr_r141_1cps.txt -cal config/cal_briken2016_r25_26.txt -wi 5000
+
+else
+    echo "usage: ./aidabuild_takechi.sh run_number subrun_number_low subrun_number_high yymmdd_hhmm"
+fi
+
+
+
+
